669aaf7f9135f3fc6718efbd7aa503cf3577a064
WillQi <williamqipizza@gmail.com>
2021-07-21 00:49:15 -0600

    add saving mcworld chunks to database implementation

--- pizzaserver-worldfile/src/main/java/io/github/willqi/pizzaserver/format/mcworld/world/chunks/MCChunkDatabase.java
+++ pizzaserver-worldfile/src/main/java/io/github/willqi/pizzaserver/format/mcworld/world/chunks/MCChunkDatabase.java
@@ -1,8 +1,14 @@
 package io.github.willqi.pizzaserver.format.mcworld.world.chunks;
 
 import io.github.willqi.pizzaserver.format.exceptions.world.chunks.NoChunkFoundException;
+import io.github.willqi.pizzaserver.nbt.streams.le.LittleEndianDataOutputStream;
+import io.github.willqi.pizzaserver.nbt.streams.nbt.NBTOutputStream;
+import io.github.willqi.pizzaserver.nbt.tags.NBTCompound;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
 import org.iq80.leveldb.DB;
 
+import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
 import java.io.IOException;
 
@@ -16,7 +22,6 @@ public class MCChunkDatabase implements Closeable {
     }
 
     public MCWorldChunk getChunk(int x, int z) throws IOException {
-
         // First extract the chunk version
         byte[] versionData = this.database.get(ChunkKey.VERSION.getLevelDBKey(x, z));
         if (versionData == null) {
@@ -64,7 +69,42 @@ public class MCChunkDatabase implements Closeable {
                 .build();
     }
 
-    public void saveChunk(MCWorldChunk bedrockChunk) {
+    public void saveChunk(MCWorldChunk bedrockChunk) throws IOException {
+        this.database.put(ChunkKey.VERSION.getLevelDBKey(bedrockChunk.getX(), bedrockChunk.getZ()), new byte[]{ (byte)bedrockChunk.getChunkVersion() });
+
+        // Data2D (height map + biome)
+        ByteBuf heightAndBiomeBuffer = ByteBufAllocator.DEFAULT.buffer(768, 768);    // 512 + 256
+        for (int height : bedrockChunk.getHeightMap()) {
+            heightAndBiomeBuffer.writeShortLE(height);
+        }
+        heightAndBiomeBuffer.writeBytes(bedrockChunk.getBiomeData());
+
+        byte[] heightAndBiomeData = new byte[768];
+        heightAndBiomeBuffer.readBytes(heightAndBiomeBuffer);
+        heightAndBiomeBuffer.release();
+        this.database.put(ChunkKey.DATA_2D.getLevelDBKey(bedrockChunk.getX(), bedrockChunk.getZ()), heightAndBiomeData);
+
+        // block entities
+        ByteArrayOutputStream blockEntityOutput = new ByteArrayOutputStream();
+        NBTOutputStream blockEntityNBTOutputStream = new NBTOutputStream(new LittleEndianDataOutputStream(blockEntityOutput));
+        for (NBTCompound blockEntityCompound : bedrockChunk.getBlockEntityNBTs()) {
+            blockEntityNBTOutputStream.writeCompound(blockEntityCompound);
+        }
+        this.database.put(ChunkKey.BLOCK_ENTITIES.getLevelDBKey(bedrockChunk.getX(), bedrockChunk.getZ()), blockEntityOutput.toByteArray());
+
+        // entities
+        ByteArrayOutputStream entityOutput = new ByteArrayOutputStream();
+        NBTOutputStream entityNBTOutputStream = new NBTOutputStream(new LittleEndianDataOutputStream(entityOutput));
+        for (NBTCompound entityCompound : bedrockChunk.getEntityNBTs()) {
+            entityNBTOutputStream.writeCompound(entityCompound);
+        }
+        this.database.put(ChunkKey.ENTITIES.getLevelDBKey(bedrockChunk.getX(), bedrockChunk.getZ()), entityOutput.toByteArray());
+
+        // sub chunks
+        for (int y = 0; y < 16; y++) {
+            byte[] subChunk = bedrockChunk.getSubChunk(y).serializeForDisk();
+            this.database.put(ChunkKey.SUB_CHUNK_DATA.getLevelDBKey(bedrockChunk.getX(), bedrockChunk.getZ(), y), subChunk);
+        }
 
     }
 
