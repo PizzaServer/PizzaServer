f715da28cfd31ffdbc792b60471e99d5c2f85e49
Will <16578982+CloudG360@users.noreply.github.com>
2021-07-24 18:06:33 +0100

    Check methods now return their val/obj parameters

--- pizzaserver-commons/src/main/java/io/github/willqi/pizzaserver/commons/utils/Check.java
+++ pizzaserver-commons/src/main/java/io/github/willqi/pizzaserver/commons/utils/Check.java
@@ -12,18 +12,24 @@ public final class Check {
      * @param obj - The object which represents the property that is missing.
      * @param loc - A string descriptor of the holding object.
      * @param name - The name of the property.
+     *
+     * @return the parameter "obj"
      */
-    public static void missingProperty(Object obj, String loc, String name) {
+    public static <T> T missingProperty(T obj, String loc, String name) {
         if(isNull(obj)) throw new MissingPropertyException(String.format("%s is missing a valid '%s' property.", loc, name));
+        return obj;
     }
 
     /**
      * Checks if a parameter is null in a cleaner way, throwing an IllegalArgumentException if true.
      * @param obj - the parameter that is potentially null.
      * @param name - the name of the parameter.
+     *
+     * @return the parameter "obj"
      */
-    public static void nullParam(Object obj, String name) {
+    public static <T> T nullParam(T obj, String name) {
         if(isNull(obj)) throw new IllegalArgumentException(String.format("'%s' cannot be null.", name));
+        return obj;
     }
 
     /**
@@ -32,18 +38,23 @@ public final class Check {
      * @param lowerBound the lower bound checked (inclusive)
      * @param upperBound the upper bound checked (inclusive)
      * @param name the name of the variable/property.
+     *
+     * @return the parameter "val"
      */
-    public static void inclusiveBounds(int val, int lowerBound, int upperBound, String name) {
+    public static int inclusiveBounds(int val, int lowerBound, int upperBound, String name) {
         inclusiveLowerBound(val, lowerBound, name);
         inclusiveUpperBound(val, upperBound, name);
+        return val;
     }
 
-    public static void inclusiveLowerBound(int val, int bound, String name) {
-        if((val < bound)) throw new IllegalStateException(String.format("'%s' is out of bounds (val = %s | Lower = %s)", name, val, bound));
+    public static int inclusiveLowerBound(int val, int bound, String name) {
+        if(val < bound) throw new IllegalStateException(String.format("'%s' is out of bounds (val = %s | Lower = %s)", name, val, bound));
+        return val;
     }
 
-    public static void inclusiveUpperBound(int val, int bound, String name) {
-        if((val < bound)) throw new IllegalStateException(String.format("'%s' is out of bounds (val = %s | Upper = %s)", name, val, bound));
+    public static int inclusiveUpperBound(int val, int bound, String name) {
+        if(val > bound) throw new IllegalStateException(String.format("'%s' is out of bounds (val = %s | Upper = %s)", name, val, bound));
+        return val;
     }
 
     public static boolean isNull(Object obj) {
