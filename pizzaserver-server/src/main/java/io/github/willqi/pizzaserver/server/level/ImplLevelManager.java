626a29ea109da6e6cab806c7fdb8bb1c43cf1aee
WillQi <williamqipizza@gmail.com>
2021-08-04 12:50:22 -0700

    fix LevelManager.close not calling unloadLevel

--- pizzaserver-server/src/main/java/io/github/willqi/pizzaserver/server/level/ImplLevelManager.java
+++ pizzaserver-server/src/main/java/io/github/willqi/pizzaserver/server/level/ImplLevelManager.java
@@ -129,7 +129,7 @@ public class ImplLevelManager implements LevelManager, Closeable {
     @Override
     public void close() throws IOException {
         for (ImplLevel level : this.levels.values()) {
-            this.unloadLevel(level.getName());
+            this.unloadLevel(level.getProvider().getFileName());
         }
     }
 }
