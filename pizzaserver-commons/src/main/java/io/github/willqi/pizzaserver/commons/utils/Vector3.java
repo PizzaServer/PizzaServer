3202743138df82356a7ebb07ff20ec12510829c9
WillQi <williamqipizza@gmail.com>
2021-08-06 19:08:12 -0600

    Implement add/subtract/multiply/divide vector math methods

--- pizzaserver-commons/src/main/java/io/github/willqi/pizzaserver/commons/utils/Vector3.java
+++ pizzaserver-commons/src/main/java/io/github/willqi/pizzaserver/commons/utils/Vector3.java
@@ -25,6 +25,46 @@ public class Vector3 {
         return this.z;
     }
 
+    public Vector3 add(Vector3 vector3) {
+        return this.add(vector3.getX(), vector3.getY(), vector3.getZ());
+    }
+
+    public Vector3 add(float x, float y, float z) {
+        return new Vector3(this.getX() + x, this.getY() + y, this.getZ() + z);
+    }
+
+    public Vector3 subtract(Vector3i vector3) {
+        return this.subtract(vector3.getX(), vector3.getY(), vector3.getZ());
+    }
+
+    public Vector3 subtract(float x, float y, float z) {
+        return new Vector3(this.getX() - x, this.getY() - y, this.getZ() - z);
+    }
+
+    public Vector3 multiply(float multiplier) {
+        return this.multiply(multiplier, multiplier, multiplier);
+    }
+
+    public Vector3 multiply(Vector3 vector3) {
+        return this.multiply(vector3.getX(), vector3.getY(), vector3.getZ());
+    }
+
+    public Vector3 multiply(float x, float y, float z) {
+        return new Vector3(this.getX() * x, this.getY() * y, this.getZ() * z);
+    }
+
+    public Vector3 divide(float divider) {
+        return this.divide(divider, divider, divider);
+    }
+
+    public Vector3 divide(Vector3 vector3) {
+        return this.divide(vector3.getX(), vector3.getY(), vector3.getZ());
+    }
+
+    public Vector3 divide(float x, float y, float z) {
+        return new Vector3(this.getX() / x, this.getY() / y, this.getZ() / z);
+    }
+
     public Vector3i toVector3i() {
         return new Vector3i((int)this.x, (int)this.y, (int)this.z);
     }
