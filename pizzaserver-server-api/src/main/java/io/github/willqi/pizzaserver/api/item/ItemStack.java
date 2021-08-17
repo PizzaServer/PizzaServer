bffb97a4df006b7017a2d777fe87dbd90fd393bf
WillQi <williamqipizza@gmail.com>
2021-08-17 01:42:28 -0600

    PlantableItemComponent

--- pizzaserver-server-api/src/main/java/io/github/willqi/pizzaserver/api/item/ItemStack.java
+++ pizzaserver-server-api/src/main/java/io/github/willqi/pizzaserver/api/item/ItemStack.java
@@ -4,6 +4,10 @@ import io.github.willqi.pizzaserver.api.item.types.BaseItemType;
 
 public class ItemStack {
 
+    private final BaseItemType itemType;
+    private int amount;
+    private int damage;
+
     public ItemStack(BaseItemType itemType) {
         this(itemType, 1);
     }
@@ -13,7 +17,29 @@ public class ItemStack {
     }
 
     public ItemStack(BaseItemType itemType, int amount, int damage) {
+        this.itemType = itemType;
+        this.amount = amount;
+        this.damage = damage;
+    }
 
+    public BaseItemType getItemType() {
+        return this.itemType;
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public void setAmount(int stackSize) {
+        this.amount = stackSize;
+    }
+
+    public int getDamage() {
+        return this.damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
     }
 
 }
