8e3149df6634946268d0ebf33780a5615af4d445
WillQi <williamqipizza@gmail.com>
2021-12-26 02:11:43 -0700

    furnace is direction based

--- pizzaserver-server-api/src/main/java/io/github/pizzaserver/api/block/types/impl/BlockTypeFurnace.java
+++ pizzaserver-server-api/src/main/java/io/github/pizzaserver/api/block/types/impl/BlockTypeFurnace.java
@@ -3,8 +3,10 @@ package io.github.pizzaserver.api.block.types.impl;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import com.nukkitx.nbt.NbtMap;
+import io.github.pizzaserver.api.block.Block;
 import io.github.pizzaserver.api.block.types.BaseBlockType;
 import io.github.pizzaserver.api.block.types.BlockTypeID;
+import io.github.pizzaserver.api.entity.Entity;
 import io.github.pizzaserver.api.item.data.ToolTier;
 import io.github.pizzaserver.api.item.data.ToolType;
 
@@ -55,6 +57,12 @@ public class BlockTypeFurnace extends BaseBlockType {
     @Override
     public ToolType getToolTypeRequired() {
         return ToolType.PICKAXE;
+    }
+
+    @Override
+    public boolean prepareForPlacement(Entity entity, Block block) {
+        block.setBlockStateIndex(entity.getHorizontalDirection().opposite().getBlockStateIndex());
+        return true;
     }
 
 }
