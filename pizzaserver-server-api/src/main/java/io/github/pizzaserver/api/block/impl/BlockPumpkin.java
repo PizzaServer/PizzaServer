e797f26586fcd8de10904cb623f6066bcd497cd6
MisledWater079 <59891258+Misledwater79@users.noreply.github.com>
2022-02-27 04:23:17 -0500

    Added block state nbt to Pumpkin blocks

--- pizzaserver-server-api/src/main/java/io/github/pizzaserver/api/block/impl/BlockPumpkin.java
+++ pizzaserver-server-api/src/main/java/io/github/pizzaserver/api/block/impl/BlockPumpkin.java
@@ -1,17 +1,47 @@
 package io.github.pizzaserver.api.block.impl;
 
+import com.nukkitx.nbt.NbtMap;
 import io.github.pizzaserver.api.block.Block;
 import io.github.pizzaserver.api.block.BlockID;
 import io.github.pizzaserver.api.item.data.ToolTier;
 import io.github.pizzaserver.api.item.data.ToolType;
+import io.github.pizzaserver.api.utils.HorizontalDirection;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BlockPumpkin extends Block {
+
+    private static final List<NbtMap> BLOCK_STATES = new ArrayList<>() {
+        {
+            for (int i = 0; i < 6; i++) {
+                this.add(NbtMap.builder()
+                        .putInt("facing_direction", i)
+                        .build());
+            }
+        }
+    };
+
+    public BlockPumpkin() { this(HorizontalDirection.NORTH); }
+
+    public BlockPumpkin(HorizontalDirection direction) { this.setDirection(direction); }
+
+    public HorizontalDirection getDirection() {
+        return HorizontalDirection.fromBlockStateIndex(this.getBlockState());
+    }
+
+    public void setDirection(HorizontalDirection direction) { this.setBlockState(direction.getBlockStateIndex()); }
 
     @Override
     public String getBlockId(){ return BlockID.PUMPKIN; }
 
     @Override
     public String getName(){ return "Pumpkin"; }
+
+    @Override
+    public List<NbtMap> getNBTStates() {
+        return BLOCK_STATES;
+    }
 
     @Override
     public float getHardness(){ return 1;}
