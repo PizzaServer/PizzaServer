7dfbf1382230f655f90af9c52266f7f1386fb24e
WillQi <williamqipizza@gmail.com>
2022-03-12 18:36:38 -0700

    mob spawner block entity

--- pizzaserver-server-api/src/main/java/io/github/pizzaserver/api/blockentity/type/BlockEntityMobSpawner.java
+++ pizzaserver-server-api/src/main/java/io/github/pizzaserver/api/blockentity/type/BlockEntityMobSpawner.java
@@ -5,6 +5,8 @@ import io.github.pizzaserver.api.blockentity.BlockEntity;
 import io.github.pizzaserver.api.entity.EntityRegistry;
 import io.github.pizzaserver.api.entity.definition.EntityDefinition;
 
+import java.util.Optional;
+
 public interface BlockEntityMobSpawner extends BlockEntity<BlockMobSpawner> {
 
     String ID = "MobSpawner";
@@ -14,7 +16,7 @@ public interface BlockEntityMobSpawner extends BlockEntity<BlockMobSpawner> {
         return ID;
     }
 
-    EntityDefinition getEntityDefinition();
+    Optional<EntityDefinition> getEntityDefinition();
 
     default void setEntityDefinition(String entityId) {
         this.setEntityDefinition(EntityRegistry.getInstance().getDefinition(entityId));
