a9cbbfffa48fa535fac5723b916dba779a9d8162
DasEtwas <18222134+DasEtwas@users.noreply.github.com>
2022-04-12 21:45:06 +0200

    add PluginBase class for ease of use

---
+++ pizzaserver-server-api/src/main/java/io/github/pizzaserver/api/plugin/PluginBase.java
@@ -0,0 +1,30 @@
+package io.github.pizzaserver.api.plugin;
+
+import io.github.pizzaserver.api.Server;
+
+/**
+ * Minimal plugin class to comply with the plugin loading interface.
+ * </p>
+ * Note that at least the {@code data.getManifest().getName()} must not change from the original value, as it is used
+ * as a key by the server.
+ */
+public class PluginBase implements Plugin {
+
+    private final Server server;
+    private final PluginData data;
+
+    public PluginBase(Server server, PluginData data) {
+        this.server = server;
+        this.data = data;
+    }
+
+    @Override
+    public Server getServer() {
+        return this.server;
+    }
+
+    @Override
+    public PluginData getData() {
+        return this.data;
+    }
+}
