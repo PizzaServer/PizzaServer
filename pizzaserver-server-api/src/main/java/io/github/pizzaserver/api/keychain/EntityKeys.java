4dff6ad3ce30b485b1e2b5256fe6f38cf5926071
Will <16578982+CloudG360@users.noreply.github.com>
2023-01-18 14:40:24 +0000

    Rename EntityKeys once again

--- pizzaserver-server-api/src/main/java/io/github/pizzaserver/api/keychain/EntityKeys.java
+++ pizzaserver-server-api/src/main/java/io/github/pizzaserver/api/keychain/EntityKeys.java
@@ -8,13 +8,6 @@ import io.github.pizzaserver.commons.data.DataKey;
 /**
  * A collection of built-in data keys, useful for accessing entity data.
  *
- * <p>
- * Action Key IDs tend to follow the names of the official query functions. If a value does not have a query function,
- * it should be replaced with <i>pizza.[appropriate name].</i> The official query functions
- * <a href="https://learn.microsoft.com/en-us/minecraft/creator/reference/content/molangreference/examples/molangconcepts/queryfunctions">
- * can be found here!
- * </a>
- * </p>
  */
 public class EntityKeys {
 
@@ -27,44 +20,54 @@ public class EntityKeys {
     // as an instance to the EntityCompImpl.
     // Maybe EntityCompImpl<T extends EntityCompDefinition> ??
 
-    //TODO: Data integrity
-    // Potentially solved the risk of data being updated at unexpected points by hiding the getContainer
-    // for stores by default. Add the listeners/callback handlers back to ImplEntity.
+    //TODO: !!!!!!!!!!! rename key ids to minecraft:blahblah
+    //TODO: Add a way to map attributes & flags to properties. See EntityFlag -
+    // Some properties would be derivable from existing data (Flag->ON_FIRE) from (EntityKeys->FIRE_TICKS_REMAINING)
+    // Not every flag should be writable. Stuff like Flag->IS_IN_UI is read-only.
+    // Some stuff can just be written an mirrored, however.
+    //TODO: See EntityData as well for more values.
+
+    // While the identifier names aren't critical, they tend to follow a convention that aligns them with
+    // entity attributes/data --> minecraft:property_here
+    // see PizzaServer-API :: AttributeType.java
+    //
+    // If a key maps to something that is really only handled by the server and won't be shared with the client,
+    // the prefix of "internal" is adopted --> internal:property_here
 
     // -- Location
-    public static final DataKey<Vector3f> POSITION = DataKey.of("query.position", Vector3f.class);
-    public static final DataKey<Float> ROTATION_PITCH = DataKey.of("query.body_x_rotation", Float.TYPE);
-    public static final DataKey<Float> ROTATION_YAW = DataKey.of("query.body_y_rotation", Float.TYPE);
-    public static final DataKey<Float> ROTATION_HEAD_PITCH = DataKey.of("query.head_x_rotation", Float.TYPE);
-    public static final DataKey<Float> ROTATION_HEAD_YAW = DataKey.of("query.head_y_rotation", Float.TYPE);
-    public static final DataKey<Float> ROTATION_HEAD_ROLL = DataKey.of("query.head_roll_angle", Float.TYPE);
-    public static final DataKey<World> WORLD = DataKey.of("pizza.entity_world", World.class);
+    public static final DataKey<Vector3f> POSITION = DataKey.of("minecraft:position", Vector3f.class);
+    public static final DataKey<Float> ROTATION_PITCH = DataKey.of("minecraft:body_x_rotation", Float.TYPE);
+    public static final DataKey<Float> ROTATION_YAW = DataKey.of("minecraft:body_y_rotation", Float.TYPE);
+    public static final DataKey<Float> ROTATION_HEAD_PITCH = DataKey.of("minecraft:head_x_rotation", Float.TYPE);
+    public static final DataKey<Float> ROTATION_HEAD_YAW = DataKey.of("minecraft:head_y_rotation", Float.TYPE);
+    public static final DataKey<Float> ROTATION_HEAD_ROLL = DataKey.of("minecraft:head_roll_angle", Float.TYPE);
+    public static final DataKey<World> WORLD = DataKey.of("internal:entity_world", World.class);
 
 
     // -- Time
-    public static final DataKey<Integer> BREATHING_TICKS_REMAINING = DataKey.of("entity_breathing_ticks", Integer.TYPE); // has a flag?
-    public static final DataKey<Integer> FIRE_TICKS_REMAINING = DataKey.of("query.on_fire_time", Integer.TYPE);
-    public static final DataKey<Integer> INVULNERABILITY_TICKS = DataKey.of("query.invulnerable_ticks", Integer.TYPE);
+    public static final DataKey<Integer> BREATHING_TICKS_REMAINING = DataKey.of("minecraft:breathing_ticks", Integer.TYPE); // has a flag?
+    public static final DataKey<Integer> FIRE_TICKS_REMAINING = DataKey.of("minecraft:on_fire_time", Integer.TYPE);
+    public static final DataKey<Integer> INVULNERABILITY_TICKS = DataKey.of("minecraft:invulnerable_ticks", Integer.TYPE);
 
 
     // -- Locomotion
-    public static final DataKey<Float> MOVEMENT_SPEED = DataKey.of("pizza.base_move_speed", Float.TYPE);
+    public static final DataKey<Float> MOVEMENT_SPEED = DataKey.of("minecraft:movement", Float.TYPE); // *Attrib
 
 
     // -- Living
-    public static final DataKey<Float> HEALTH = DataKey.of("query.health", Float.TYPE);
-    public static final DataKey<Float> ABSORPTION = DataKey.of("pizza.absorption", Float.TYPE);
-    public static final DataKey<Boolean> IS_VULNERABLE = DataKey.of("pizza.is_vulnerable", Boolean.TYPE);
+    public static final DataKey<Float> HEALTH = DataKey.of("minecraft:health", Float.TYPE); // *Attrib
+    public static final DataKey<Float> ABSORPTION = DataKey.of("minecraft:absorption", Float.TYPE); // *Attrib
+    public static final DataKey<Boolean> IS_VULNERABLE = DataKey.of("minecraft:is_vulnerable", Boolean.TYPE);
 
 
     // -- Humanoid
-    public static final DataKey<Float> FOOD = DataKey.of("pizza.food", Float.TYPE);
-    public static final DataKey<Float> SATURATION = DataKey.of("pizza.saturation", Float.TYPE);
-    public static final DataKey<Float> XP = DataKey.of("pizza.entity_xp", Float.TYPE);
-    public static final DataKey<Float> XP_LEVELS = DataKey.of("pizza.entity_xp_levels", Float.TYPE);
+    public static final DataKey<Float> FOOD = DataKey.of("minecraft:player.hunger", Float.TYPE); // *Attrib
+    public static final DataKey<Float> SATURATION = DataKey.of("minecraft:player.saturation", Float.TYPE); // *Attrib
+    public static final DataKey<Float> XP = DataKey.of("minecraft:player.experience", Float.TYPE); // *Attrib
+    public static final DataKey<Float> XP_LEVELS = DataKey.of("minecraft:player.level", Float.TYPE); // *Attrib
 
 
     // -- Misc
-    public static final DataKey<BossBar> BOSS_BAR = DataKey.of("pizza.boss_bar", BossBar.class);
+    public static final DataKey<BossBar> BOSS_BAR = DataKey.of("internal:boss_bar", BossBar.class);
 
 }
