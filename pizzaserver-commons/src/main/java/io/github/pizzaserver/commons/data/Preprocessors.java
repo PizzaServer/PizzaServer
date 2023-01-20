2eca8cee1a18531d4bf19ff9db54312225ee4f17
Will <16578982+CloudG360@users.noreply.github.com>
2023-01-20 21:24:54 +0000

    Added a way to mix preprocessors

--- pizzaserver-commons/src/main/java/io/github/pizzaserver/commons/data/Preprocessors.java
+++ pizzaserver-commons/src/main/java/io/github/pizzaserver/commons/data/Preprocessors.java
@@ -14,4 +14,38 @@ public class Preprocessors {
         return data -> Check.isNull(data) ? alternative : data;
     }
 
+
+    public static <T extends Number> Function<T, T> ensureAboveValue(T minimum) {
+        return data -> minimum.floatValue() > data.floatValue()
+                ? minimum
+                : data;
+    }
+
+    public static <T extends Number> Function<T, T> ensureBelowValue(T maximum) {
+        return data -> data.floatValue() > maximum.floatValue()
+                ? maximum
+                : data;
+    }
+
+    public static <T extends Number> Function<T, T> ensureAboveValue(ValueInterface<T> minimum) {
+        return Preprocessors.ensureAboveValue(minimum.getValue());
+    }
+
+    public static <T extends Number> Function<T, T> ensureBelowValue(ValueInterface<T> maximum) {
+        return Preprocessors.ensureBelowValue(maximum.getValue());
+    }
+
+
+
+    public static <T> Function<T, T> inOrder(Function<T, T>... stack) {
+        return data -> {
+            T feed = data;
+
+            for (int i = 0; i < stack.length; i++)
+                feed = stack[i].apply(feed);
+
+            return feed;
+        };
+    }
+
 }
