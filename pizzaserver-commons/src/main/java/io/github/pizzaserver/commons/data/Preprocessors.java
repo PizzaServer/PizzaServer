c060d7fb9b99bc3fd2ed7e0071ddd0096d8f2637
Will <16578982+CloudG360@users.noreply.github.com>
2023-02-07 21:20:27 +0000

    Avoided a potential headache with data - renamed functions to indicate the difference between substitutes that don't change and substitutes that are obtained during runtime

--- pizzaserver-commons/src/main/java/io/github/pizzaserver/commons/data/Preprocessors.java
+++ pizzaserver-commons/src/main/java/io/github/pizzaserver/commons/data/Preprocessors.java
@@ -3,6 +3,7 @@ package io.github.pizzaserver.commons.data;
 import io.github.pizzaserver.commons.utils.Check;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class Preprocessors {
 
@@ -10,39 +11,58 @@ public class Preprocessors {
         return data -> Check.notNull(data, name);
     }
 
-    public static <T> Function<T, T> ifNullThen(T alternative) {
+    public static <T> Function<T, T> ifNullThenConstant(T alternative) {
         return data -> Check.isNull(data) ? alternative : data;
     }
 
+    public static <T> Function<T, T> ifNullThenValue(Supplier<T> alternative) {
+        return data -> Check.isNull(data) ? alternative.get() : data;
+    }
 
-    public static <T extends Number> Function<T, T> ensureAboveValue(T minimum) {
+
+    public static <T extends Number> Function<T, T> ensureAboveConstant(T minimum) {
         return data -> minimum.floatValue() > data.floatValue()
                 ? minimum
                 : data;
     }
 
-    public static <T extends Number> Function<T, T> ensureBelowValue(T maximum) {
+    public static <T extends Number> Function<T, T> ensureAboveValue(Supplier<T> minimum) {
+        T min = minimum.get();
+        return data -> min.floatValue() > data.floatValue()
+                ? min
+                : data;
+    }
+
+    public static <T extends Number> Function<T, T> ensureBelowConstant(T maximum) {
         return data -> data.floatValue() > maximum.floatValue()
                 ? maximum
                 : data;
     }
 
+    public static <T extends Number> Function<T, T> ensureBelowValue(Supplier<T> maximum) {
+        T max = maximum.get();
+        return data -> data.floatValue() > max.floatValue()
+                ? max
+                : data;
+    }
+
     public static <T extends Number> Function<T, T> ensureAboveValue(ValueInterface<T> minimum) {
-        return Preprocessors.ensureAboveValue(minimum.getValue());
+        return Preprocessors.ensureAboveValue(minimum::getValue);
     }
 
     public static <T extends Number> Function<T, T> ensureBelowValue(ValueInterface<T> maximum) {
-        return Preprocessors.ensureBelowValue(maximum.getValue());
+        return Preprocessors.ensureBelowValue(maximum::getValue);
     }
 
 
 
+    @SafeVarargs
     public static <T> Function<T, T> inOrder(Function<T, T>... stack) {
         return data -> {
             T feed = data;
 
-            for (int i = 0; i < stack.length; i++)
-                feed = stack[i].apply(feed);
+            for (Function<T, T> ttFunction : stack)
+                feed = ttFunction.apply(feed);
 
             return feed;
         };
