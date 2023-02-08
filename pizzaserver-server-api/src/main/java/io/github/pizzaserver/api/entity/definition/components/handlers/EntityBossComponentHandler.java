dd1ccecedc9a81d656a2e0dc6f9236422dafb4b4
Will <16578982+CloudG360@users.noreply.github.com>
2023-02-08 01:18:51 +0000

    Fixed remaining changes related to key-based data

--- pizzaserver-server-api/src/main/java/io/github/pizzaserver/api/entity/definition/components/handlers/EntityBossComponentHandler.java
+++ pizzaserver-server-api/src/main/java/io/github/pizzaserver/api/entity/definition/components/handlers/EntityBossComponentHandler.java
@@ -5,6 +5,7 @@ import io.github.pizzaserver.api.entity.Entity;
 import io.github.pizzaserver.api.entity.boss.BossBar;
 import io.github.pizzaserver.api.entity.definition.components.EntityComponentHandler;
 import io.github.pizzaserver.api.entity.definition.components.impl.EntityBossComponent;
+import io.github.pizzaserver.api.keychain.EntityKeys;
 
 public class EntityBossComponentHandler extends EntityComponentHandler<EntityBossComponent> {
 
@@ -12,10 +13,14 @@ public class EntityBossComponentHandler extends EntityComponentHandler<EntityBos
     public void onRegistered(Entity entity, EntityBossComponent component) {
         BossBar bossBar = null;
         if (component.isEnabled()) {
+            float health = entity.expect(EntityKeys.HEALTH);
+            float maxHealth = entity.expect(EntityKeys.MAX_HEALTH);
+            float percentage = health / (maxHealth < 0 ? maxHealth : 1);
+
             bossBar = Server.getInstance().createBossBar();
             bossBar.setTitle(component.getBossName().orElse(entity.getName()));
             bossBar.setDarkenSky(component.shouldDarkenSky());
-            bossBar.setPercentage(entity.getHealth() / (entity.getMaxHealth() != 0 ? entity.getMaxHealth() : 1));
+            bossBar.setPercentage(percentage);
             bossBar.setRenderRange(component.getRange());
         }
 
